50577324cabc5e3dd458d28f501c9b3463c1f427
Rainer Stropek <rainer@software-architects.at>
2020-07-08 12:43:44 +0200

    Some enhancements

--- VatIDChecker/VatIDChecker/VatIDCheck.cs
+++ VatIDChecker/VatIDChecker/VatIDCheck.cs
@@ -10,6 +10,7 @@ using System.Net.Http;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
+using System.Web.Http;
 using System.Xml;
 using System.Xml.Linq;
 using System.Xml.XPath;
@@ -19,7 +20,6 @@ namespace VatIDChecker
     public class VatIDCheck
     {
         private readonly HttpClient client;
-        public ValidationParams valParam = new ValidationParams();
 
         public VatIDCheck(IHttpClientFactory clientFactory)
         {
@@ -31,91 +31,107 @@ namespace VatIDChecker
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            // For details about the structure of the HTTP request sent
-            // from Billomat see https://www.billomat.com/api/webhooks/.
-            var clientId = await GetClientIdFromRequestBody(req.Body);
-            if (string.IsNullOrEmpty(clientId))
+            try
             {
-                log.LogError("Cannot find client id in JSON body.");
-                return new BadRequestResult();
-            }
-
-            var billomatClient = await GetClientFromBillomat(clientId);
-            var countryCode = billomatClient.country_code;
-            var vatNumber = billomatClient.vat_number.Substring(2).Replace(" ", string.Empty);
-            var clientName = billomatClient.name;
-            var street = billomatClient.street;
-            var zip = billomatClient.zip;
-            var city = billomatClient.city;
-            var clientAddress = street + " " + countryCode + "-" + zip + " " + city;
-
-            var xmlContent = await PostXMLToEU(countryCode, vatNumber);
-            var soapResponse = XDocument.Parse(xmlContent.ToString());
-            GetValidEUParam(soapResponse);
-
-            // UST_ID Validation
-            var userResponse = string.Empty;
-            if (valParam.valid == null)
-            {
-                userResponse = "Not found";
-            }
-            else
-            {
-                if (valParam.valid == "true")
+                // For details about the structure of the HTTP request sent
+                // from Billomat see https://www.billomat.com/api/webhooks/.
+                var clientId = await GetClientIdFromRequestBody(req.Body);
+                if (string.IsNullOrEmpty(clientId))
                 {
-                    if ((valParam.name != null) && (valParam.name.ToLower().Replace("\n", " ") == clientName.ToLower().Replace("\n", " ")) && (valParam.name != "---"))
-                    {
-                        userResponse = "Correct company name: " + valParam.name;
-                    }
-                    else
-                    {
-                        userResponse += "Company name not valid";
-                    }
-                    if ((valParam.address != null) && (valParam.address.ToLower().Replace("\n", " ") == clientAddress.ToLower().Replace("\n", " ")) && (valParam.address != "---"))
-                    {
-                        userResponse += "\nCorrect address: " + valParam.address.Replace("\n", " ");
-                    }
-                    else
-                    {
-                        userResponse += "\nAddress not valid";
-                    }
-                    if ((valParam.cCode != null) && (valParam.cCode != "---") && (valParam.cCode == countryCode))
-                    {
-                        userResponse += "\nCorrect country code: " + valParam.cCode;
-                    }
-                    else
-                    {
-                        userResponse += "\nCountry Code not valid";
-                    }
-                    if ((valParam.vatNum != null) && (valParam.vatNum != "---") && (valParam.vatNum == vatNumber))
-                    {
-                        userResponse += "\nCorrect vat-number: " + valParam.vatNum;
-                    }
-                    else
-                    {
-                        userResponse += "\nVatNumber not valid";
-                    }
+                    log.LogError("Cannot find client id in JSON body.");
+                    return new BadRequestResult();
+                }
+
+                var billomatClient = await GetClientFromBillomat(clientId);
+                var countryCode = billomatClient.country_code;
+                var vatNumber = billomatClient.vat_number.Substring(2).Replace(" ", string.Empty);
+                var clientName = billomatClient.name;
+                var street = billomatClient.street;
+                var zip = billomatClient.zip;
+                var city = billomatClient.city;
+                var clientAddress = street + " " + countryCode + "-" + zip + " " + city;
+
+                var xmlContent = await PostXMLToEU(countryCode, vatNumber);
+                var soapResponse = XDocument.Parse(xmlContent.ToString());
+                var valParam = GetValidEUParam(soapResponse);
+
+                // UST_ID Validation
+                var userResponse = string.Empty;
+                if (valParam.valid == null)
+                {
+                    userResponse = "Not found";
                 }
                 else
                 {
-                    userResponse = "Not valid";
+                    if (valParam.valid == "true")
+                    {
+                        if ((valParam.name != null) && (valParam.name.ToLower().Replace("\n", " ") == clientName.ToLower().Replace("\n", " ")) && (valParam.name != "---"))
+                        {
+                            userResponse = "Correct company name: " + valParam.name;
+                        }
+                        else
+                        {
+                            userResponse += "Company name not valid";
+                        }
+                        if ((valParam.address != null) && (valParam.address.ToLower().Replace("\n", " ") == clientAddress.ToLower().Replace("\n", " ")) && (valParam.address != "---"))
+                        {
+                            userResponse += "\nCorrect address: " + valParam.address.Replace("\n", " ");
+                        }
+                        else
+                        {
+                            userResponse += "\nAddress not valid";
+                        }
+                        if ((valParam.cCode != null) && (valParam.cCode != "---") && (valParam.cCode == countryCode))
+                        {
+                            userResponse += "\nCorrect country code: " + valParam.cCode;
+                        }
+                        else
+                        {
+                            userResponse += "\nCountry Code not valid";
+                        }
+                        if ((valParam.vatNum != null) && (valParam.vatNum != "---") && (valParam.vatNum == vatNumber))
+                        {
+                            userResponse += "\nCorrect vat-number: " + valParam.vatNum;
+                        }
+                        else
+                        {
+                            userResponse += "\nVatNumber not valid";
+                        }
+                    }
+                    else
+                    {
+                        userResponse = "Not valid";
+                    }
                 }
+                return new OkObjectResult(userResponse);
             }
-            return new OkObjectResult(userResponse);
+            catch (Exception ex)
+            {
+                log.LogError(ex, "Error while checking VAT ID");
+                return new InternalServerErrorResult();
+            }
+
         }
-        private void GetValidEUParam(XDocument soapRes)
+
+        private ValidationParams GetValidEUParam(XDocument soapRes)
         {
             var nameTable = new NameTable();
             var nsManager = new XmlNamespaceManager(nameTable);
             nsManager.AddNamespace("x", "urn:ec.europa.eu:taxud:vies:services:checkVat:types");
             nsManager.AddNamespace("soap", "http://schemas.xmlsoap.org/soap/envelope/");
 
-            valParam.valid = (string)soapRes.XPathSelectElement("//soap:Body/x:checkVatResponse/x:valid", nsManager);
-            valParam.name = (string)soapRes.XPathSelectElement("//soap:Body/x:checkVatResponse/x:name", nsManager);
-            valParam.address = (string)soapRes.XPathSelectElement("//soap:Body/x:checkVatResponse/x:address", nsManager);
-            valParam.cCode = (string)soapRes.XPathSelectElement("//soap:Body/x:checkVatResponse/x:countryCode", nsManager);
-            valParam.vatNum = (string)soapRes.XPathSelectElement("//soap:Body/x:checkVatResponse/x:vatNumber", nsManager);
+            var valParam = new ValidationParams()
+            {
+                valid = (string)soapRes.XPathSelectElement("//soap:Body/x:checkVatResponse/x:valid", nsManager),
+                name = (string)soapRes.XPathSelectElement("//soap:Body/x:checkVatResponse/x:name", nsManager),
+                address = (string)soapRes.XPathSelectElement("//soap:Body/x:checkVatResponse/x:address", nsManager),
+                cCode = (string)soapRes.XPathSelectElement("//soap:Body/x:checkVatResponse/x:countryCode", nsManager),
+                vatNum = (string)soapRes.XPathSelectElement("//soap:Body/x:checkVatResponse/x:vatNumber", nsManager)
+            };
+
+            return valParam;
         }
+
         private async Task<string> GetClientIdFromRequestBody(Stream body)
         {
             var requestBody = await new StreamReader(body).ReadToEndAsync();
@@ -151,6 +167,8 @@ namespace VatIDChecker
             };
 
             var postResponse = await client.SendAsync(webPostRequest);
+            postResponse.EnsureSuccessStatusCode();
+
             var postContent = postResponse.Content;
             var postXmlContent = postContent.ReadAsStringAsync().Result;
             return postXmlContent;
@@ -177,6 +195,8 @@ namespace VatIDChecker
             };
 
             using var getResponse = await client.SendAsync(webGetRequest);
+            getResponse.EnsureSuccessStatusCode();
+
             var getContent = getResponse.Content;
             var getJsonContent = getContent.ReadAsStringAsync().Result;
 
