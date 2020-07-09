3b988554e76b9feedf6a13748aaa620a5c5f4a02
UndeMe <40146720170130@litec.ac.at>
2020-07-09 09:17:36 +0200

    minor changes

--- VatIDChecker/VatIDChecker.Tests/ValidateVatInformationTests.cs
+++ VatIDChecker/VatIDChecker.Tests/ValidateVatInformationTests.cs
@@ -1,5 +1,4 @@
 using Moq;
-using System;
 using System.Net.Http;
 using Xunit;
 
--- VatIDChecker/VatIDChecker/VatIDCheck.cs
+++ VatIDChecker/VatIDChecker/VatIDCheck.cs
@@ -7,7 +7,6 @@ using System;
 using System.IO;
 using System.Net;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
